7d039a70556e5166e768bfb03c09885f1bef7d07
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-02-27 14:33:54 +0330

    fixed navbar is Responsive success

--- src/components/module/Navbar/Navbar.tsx
+++ src/components/module/Navbar/Navbar.tsx
@@ -2,10 +2,18 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
-import {FaSearch, FaShoppingCart, FaTimes } from 'react-icons/fa'
+import {FaBars, FaSearch, FaShoppingCart, FaTimes } from 'react-icons/fa'
 
 const Navbar:React.FC =()=>{
     const [activeSearch , setActiveSearch] = useState<boolean>(false);
+    const [activeShowMenu , setActiveShowMenu] = useState<boolean>(false)
+
+    const handlerShowMenu:React.ReactEventHandler = ()=>{
+        setActiveShowMenu(true)
+    }
+    const handlerExitMenu:React.ReactEventHandler = ()=>{
+        setActiveShowMenu(false)
+    }
 
     const handlerShowSearch:React.ReactEventHandler = ()=>{
         setActiveSearch(!activeSearch)
@@ -15,7 +23,8 @@ const Navbar:React.FC =()=>{
     }
 
   return (
-    <section className="block bg-white py-4 border-b border-b-secondary-100 relative">
+        <>
+             <section className="hidden lg:block bg-white py-4 border-b border-b-secondary-100 relative">
         <div className="container">
             <div className="flex items-center justify-between">
                 <ul className="flex gap-4">
@@ -76,7 +85,72 @@ const Navbar:React.FC =()=>{
             </div>
         </div>
           
-    </section>
+              </section>
+
+              <section className="block lg:hidden bg-white py-4 border-b border-b-secondary-100 relative">
+                    <div className="container">
+                       <div className="flex items-center justify-between">
+                       <span className="block text-3xl text-text duration-300 ease-linear hover:text-secondary-100 cursor-pointer"
+                       onClick={handlerShowMenu}
+                       >
+                            <FaBars/>
+                        </span>
+                        {activeShowMenu && (
+                            <>
+                            <div className="bg_shadow text-white"></div>
+                                <div className="block fixed top-0 right-0 bg-white w-[300px] h-full z-[90]">
+                                    <div className="flex justify-between items-center p-5">
+                                    <Link href='/' className='block font-iranMedium font-semibold text-xl text-text duration-300 ease-linear hover:text-secondary-100 hover:border-b border-b-primary-200'>ورود | ثبت نام</Link>
+                                        <span className="block text-2xl text-text duration-300 ease-linear hover:text-secondary-100 cursor-pointer"
+                                        onClick={handlerExitMenu}
+                                        >
+                                            <FaTimes/>
+                                        </span>
+                                    </div>
+                                <div className="flex justify-between items-center px-4 h-12 bg-secondary-300">
+                                    <input type="text" placeholder='جستجتو محصول' className='outline-none border-none placeholder:font-iranBold' />
+                                    <span className="block cursor-pointer">
+                                    <FaSearch/>
+                                    </span>
+                                </div>
+                                <ul className="block mt-8">
+                                    <li className="block mb-4">
+                                        <Link href='' className='block font-iranMedium font-semibold text-xl text-text duration-300 ease-linear hover:text-secondary-100'>خانه</Link>
+                                    </li>
+                                    <li className="block mb-4">
+                                        <Link href='' className='block font-iranMedium font-semibold text-xl text-text duration-300 ease-linear hover:text-secondary-100'>قهوه ملو</Link>
+                                    </li>
+                                    <li className="block mb-4">
+                                        <Link href='' className='block font-iranMedium font-semibold text-xl text-text duration-300 ease-linear hover:text-secondary-100'>قهوه خارجی</Link>
+                                    </li>
+                                    <li className="block mb-4">
+                                        <Link href='' className='block font-iranMedium font-semibold text-xl text-text duration-300 ease-linear hover:text-secondary-100'>قهوه فوری</Link>
+                                    </li>
+                                    <li className="block mb-4">
+                                        <Link href='' className='block font-iranMedium font-semibold text-xl text-text duration-300 ease-linear hover:text-secondary-100'>قهوه ساز</Link>
+                                    </li>
+                                    <li className="block">
+                                        <Link href='' className='block font-iranMedium font-semibold text-xl text-text duration-300 ease-linear hover:text-secondary-100'>لوازم جانبی</Link>
+                                    </li>
+                                </ul>
+                                </div>
+                            </>
+                        )}
+                        
+                        <Link href='/'>
+                            <Image src='/assets/images/logo.png' width={100} height={100} alt='logo image'/>
+                        </Link>
+
+                        <Link href='' className='flex justify-center items-center text-secondary-100 text-2xl w-12 h-12 rounded-full border border-secondary-100 relative duration-300 ease-linear hover:text-primary-100'>
+                            <span className="block">
+                                <FaShoppingCart/>
+                            </span>
+                            <span className="flex justify-center items-center bg-white w-6 h-6  text-sm shadow shadow-secondary-100 rounded-full absolute -top-3 -left-1">0</span>
+                        </Link>
+                       </div>
+                    </div>
+              </section>
+        </>
   )
 }
 
