9da7f4e357d1b1a3b1714c981a7a6ec85ae6c7d0
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-09 14:26:09 +0330

    created page ProductSingle

---
+++ src/app/shop/[id]/page.tsx
@@ -0,0 +1,18 @@
+import Breadcrumb from '@/components/module/Breadcrumb/Breadcrumb'
+import Footer from '@/components/module/Footer/Footer'
+import MainHeader from '@/components/module/MainHeader/MainHeader'
+import Navbar from '@/components/module/Navbar/Navbar'
+import React from 'react'
+
+const page:React.FC = ()=>{
+  return (
+    <>
+    <Navbar/>
+    <MainHeader/>
+    <Breadcrumb text="جزئیات محصول"/>
+    <Footer/>
+    </>
+  )
+}
+
+export default page
