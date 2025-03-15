0a8875a2105836f0ddef80f3616b7e615bf57024
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-15 13:56:40 +0330

    created page single-article

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,20 @@
+import Breadcrumb from '@/components/module/Breadcrumb/Breadcrumb';
+import Footer from '@/components/module/Footer/Footer';
+import MainHeader from '@/components/module/MainHeader/MainHeader';
+import Navbar from '@/components/module/Navbar/Navbar';
+import React from 'react';
+
+const page:React.FC =  ()=>{
+  return (
+    <>
+    <Navbar/>
+    <MainHeader/>
+    <Breadcrumb 
+    text='مقاله'
+    />
+    <Footer/>
+    </>
+  )
+}
+
+export default page
