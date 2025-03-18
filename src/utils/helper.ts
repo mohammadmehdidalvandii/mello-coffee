904d18dd841d753c27fb309ad94263d391b71786
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-18 17:09:22 +0330

    created showAlert

---
+++ src/utils/helper.ts
@@ -0,0 +1,12 @@
+import swal from 'sweetalert';
+
+
+const showAlert = (title:string , icon:string , buttons?:string) => {
+    return swal({
+        title:title,
+        icon:icon,
+        buttons:buttons
+    });
+}
+
+export { showAlert };
