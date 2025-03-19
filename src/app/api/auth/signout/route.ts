6e985fdcd284cdcb9c44fe43064374f727286878
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-19 14:44:02 +0330

    created logic User-logout

---
+++ src/app/api/auth/signout/route.ts
@@ -0,0 +1,10 @@
+import { cookies } from "next/headers";
+
+export async function POST (){
+    cookies().delete("token");
+    return Response.json(   
+        {
+            "message":"Logout is Done "
+        }
+    )
+}
