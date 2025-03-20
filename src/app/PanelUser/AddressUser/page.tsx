952bcc8ea041f588a65801dd4d18acb9db70c593
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-20 14:06:31 +0330

    created page panelUser/AddressUser

---
+++ src/app/PanelUser/AddressUser/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@/components/layout/UserPanelLayout';
+
+const page:React.FC = ()=>{
+  return (
+    <Layout>
+        this is address user
+    </Layout>
+  )
+}
+
+export default page
