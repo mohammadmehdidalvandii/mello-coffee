c4a840bf848ef1fc5d50ccbe3090fd8aa539810d
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-24 14:01:29 +0330

    created section AdminAccount for AdminPanel

--- src/app/PanelAdmin/Account/page.tsx
+++ src/app/PanelAdmin/Account/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '@/components/layout/AdminPanelLayout';
+import AdminAccount from '@/components/template/p-admin/AdminAccount/AdminAccount';
 
 const page:React.FC = ()=>{
   return (
        <Layout>
-            this is Account admin
+        <AdminAccount/>
        </Layout> 
   )
 }
