a8421e55c4961a4b4fecc3a22c5617f794bd8ebf
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-24 15:26:17 +0330

    create api product put

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -20,3 +20,56 @@ export async function DELETE(req:NextRequest, {params}){
         )
     }
 }
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        await connectToDB();
+        const productID = params.id;
+        const formData = await req.formData();
+
+        const image = formData.get("image") as File;
+        let imageUrl = "";
+
+        if (image) {
+            imageUrl = image.name;
+        }
+
+        const updateData = {
+            name: formData.get("name")?.toString(),
+            main_description: formData.get("main_description")?.toString(),
+            description: formData.get("description")?.toString(),
+            price: formData.get("price")?.toString(),
+            image: imageUrl,
+            category: formData.get("category")?.toString(),
+            explain: formData.get("explain")?.toString(),
+            compound: formData.get("compound")?.toString(),
+            bitterness: formData.get("bitterness")?.toString(),
+            taste: formData.get("taste")?.toString()
+        };
+
+        const product = await ProductModel.findByIdAndUpdate(
+            productID,
+            updateData,
+            { new: true }
+        );
+
+        if (!product) {
+            return Response.json(
+                { message: "Product not found" },
+                { status: 404 }
+            );
+        }
+
+        return Response.json(
+            { message: "Product updated successfully", product },
+            { status: 200 }
+        );
+
+    } catch (err) {
+        console.log("Error updating product:", err);
+        return Response.json(
+            { message: "Error updating product" },
+            { status: 500 }
+        );
+    }
+}
