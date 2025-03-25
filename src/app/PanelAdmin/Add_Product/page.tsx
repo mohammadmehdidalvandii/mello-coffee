44f9afbe08a27297132b6519ab7b6569a98aac47
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-25 14:21:07 +0330

    created page add-Product form adminPanel

---
+++ src/app/PanelAdmin/Add_Product/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '@/components/layout/AdminPanelLayout'
+
+function page() {
+  return (
+    <Layout>
+        this is add product
+    </Layout>
+  )
+}
+
+export default page
