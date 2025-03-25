26c540aa9f7aafb51aa3639d39180278fe98d53e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-25 14:58:33 +0330

    fixed api get users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,6 +3,7 @@ import connectToDB from '@/config/db';
 import { NextRequest } from 'next/server';
 
 export async function GET(req:NextRequest ){
+    connectToDB()
     const users = await UserModel.find();
     return Response.json(
         {message:"get all Users" , users},
